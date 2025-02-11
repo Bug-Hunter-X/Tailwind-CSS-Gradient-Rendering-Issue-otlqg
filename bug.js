8cc1147131689668af175ae3a2c102c7d4b314f9
BugHunter-X <askzenai@gmail.com>
2025-02-11 21:50:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  <p>Some text here</p>
+</div>
+```
+This code uses Tailwind CSS classes to create a gradient background. However, sometimes this gradient might not render correctly, appearing as a solid color or not at all, especially in older browsers or with certain CSS configurations.
